b46dde472f85b09448a5020f38f247763d9ea401
Shashank <shashank3fr@gmail.com>
2026-04-26 23:55:43 +0530

    fix: invalid  port

--- backend/bootstrap/app.go
+++ backend/bootstrap/app.go
@@ -29,7 +29,7 @@ func StartApp() {
 
 	handler := c.Handler(router)
 
-	addr := "0.0.0.0:" + port + port
+	addr := "0.0.0.0:" + port
 	log.Println("server started on", addr)
 
 	if err := http.ListenAndServe(addr, handler); err != nil {
